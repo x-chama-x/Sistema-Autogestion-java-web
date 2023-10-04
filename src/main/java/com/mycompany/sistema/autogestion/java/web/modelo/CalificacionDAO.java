a2ee94285682423a7a4c124a7f2991b418d88e53
Manuel Botas <manuelbotas2001@gmail.com>
2023-10-04 04:00:33 -0300

    feat: agregada la clase CalificacionDAO

---
+++ src/main/java/com/mycompany/sistema/autogestion/java/web/modelo/CalificacionDAO.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.sistema.autogestion.java.web.modelo;
+
+import java.util.List;
+
+/**
+ *
+ * @author Manuel Botas
+ */
+public class CalificacionDAO implements	DAO<Calificacion, Integer> {
+        @Override
+        public void insertar(Calificacion entidad) throws Exception {
+            throw new UnsupportedOperationException("Not supported yet.");
+        }
+
+        @Override
+        public void modificar(Calificacion entidad) throws Exception {
+            throw new UnsupportedOperationException("Not supported yet.");
+        }
+
+        @Override
+        public void eliminar(Calificacion id) throws Exception {
+            throw new UnsupportedOperationException("Not supported yet.");
+        }
+
+        @Override
+        public List<Calificacion> listar() throws Exception {
+            throw new UnsupportedOperationException("Not supported yet.");
+        }
+
+        @Override
+        public Calificacion buscar(Integer id) throws Exception {
+            throw new UnsupportedOperationException("Not supported yet.");
+        }
+}
