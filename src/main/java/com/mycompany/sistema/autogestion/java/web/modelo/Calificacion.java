37c4909a4377a55930c330461fc87cc09b242e24
Manuel Botas <manuelbotas2001@gmail.com>
2023-10-04 05:01:33 -0300

    feat: agregados getters y setters a la clase Calificacion

--- src/main/java/com/mycompany/sistema/autogestion/java/web/modelo/Calificacion.java
+++ src/main/java/com/mycompany/sistema/autogestion/java/web/modelo/Calificacion.java
@@ -8,11 +8,35 @@ package com.mycompany.sistema.autogestion.java.web.modelo;
 public class Calificacion {
     private int idCalificacion;
     private int nota;
-    private int num_examen;
+    private int numExamen;
 
-    public Calificacion(int idCalificacion, int nota, int num_examen) {
+    public Calificacion(int idCalificacion, int nota, int numExamen) {
         this.idCalificacion = idCalificacion;
         this.nota = nota;
-        this.num_examen = num_examen;
+        this.numExamen = numExamen;
+    }
+
+    public int getIdCalificacion() {
+        return idCalificacion;
+    }
+
+    public void setIdCalificacion(int idCalificacion) {
+        this.idCalificacion = idCalificacion;
+    }
+
+    public int getNota() {
+        return nota;
+    }
+
+    public void setNota(int nota) {
+        this.nota = nota;
+    }
+
+    public int getNumExamen() {
+        return numExamen;
+    }
+
+    public void setNumExamen(int numExamen) {
+        this.numExamen = numExamen;
     }
 }
