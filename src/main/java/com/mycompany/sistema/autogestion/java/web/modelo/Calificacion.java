458325ed8e4eaea08a3cf86db96a69f9ce0b586f
Manuel Botas <manuelbotas2001@gmail.com>
2023-10-04 04:26:33 -0300

    feat: agregado constructor a la clase Calificacion

--- src/main/java/com/mycompany/sistema/autogestion/java/web/modelo/Calificacion.java
+++ src/main/java/com/mycompany/sistema/autogestion/java/web/modelo/Calificacion.java
@@ -9,4 +9,10 @@ public class Calificacion {
     private int idCalificacion;
     private int nota;
     private int num_examen;
+
+    public Calificacion(int idCalificacion, int nota, int num_examen) {
+        this.idCalificacion = idCalificacion;
+        this.nota = nota;
+        this.num_examen = num_examen;
+    }
 }
