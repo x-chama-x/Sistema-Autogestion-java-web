da4f2dcd6bcfadb78a8fcf98437b4511cfd2ca8e
Manuel Botas <manuelbotas2001@gmail.com>
2023-10-04 02:12:04 -0300

    feat: agregada la clase Calificacion y sus atributos

---
+++ src/main/java/com/mycompany/sistema/autogestion/java/web/modelo/Calificacion.java
@@ -0,0 +1,14 @@
+
+package com.mycompany.sistema.autogestion.java.web.modelo;
+
+/**
+ *
+ * @author Charly Cimino
+ * Aprendé más Java en mi canal: https://www.youtube.com/c/CharlyCimino
+ * Encontrá más código en mi repo de GitHub: https://github.com/CharlyCimino
+ */
+public class Calificacion {
+    private int idCalificacion;
+    private int nota;
+    private int num_examen;
+}
