bdd9fcff24ad390187dca6b217c3a0b539bfb0ef
Manuel Botas <manuelbotas2001@gmail.com>
2023-10-25 22:03:11 -0300

    feat: sobreescritos los métodos de DAO en MateriaDAO

--- src/main/java/com/mycompany/sistema/autogestion/java/web/modelo/MateriaDAO.java
+++ src/main/java/com/mycompany/sistema/autogestion/java/web/modelo/MateriaDAO.java
@@ -4,10 +4,37 @@
  */
 package com.mycompany.sistema.autogestion.java.web.modelo;
 
+import java.util.List;
+
 /**
  *
  * @author Manuel Botas
  */
-public class MateriaDAO {
+public class MateriaDAO implements DAO<Materia, Integer> {
+
+    @Override
+    public void insertar(Materia entidad) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void modificar(Materia entidad) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void eliminar(Materia id) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public List<Materia> listar() throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Materia buscar(Integer id) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
     
 }
