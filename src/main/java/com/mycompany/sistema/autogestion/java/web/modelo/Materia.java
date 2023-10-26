661f93c3e626ee6c83eb56deefb2a93d9751a15e
Manuel Botas <manuelbotas2001@gmail.com>
2023-10-25 23:15:21 -0300

    feat: agregados getters y setters a Materia

--- src/main/java/com/mycompany/sistema/autogestion/java/web/modelo/Materia.java
+++ src/main/java/com/mycompany/sistema/autogestion/java/web/modelo/Materia.java
@@ -16,6 +16,20 @@ public class Materia {
         this.idMateria = idMateria;
         this.nombre = nombre;
     }
-    
-    
+
+    public int getIdMateria() {
+        return idMateria;
+    }
+
+    public void setIdMateria(int idMateria) {
+        this.idMateria = idMateria;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 }
