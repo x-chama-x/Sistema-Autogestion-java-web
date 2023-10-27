4b4d85f9436be273f0a6cd00b77f2510784470e6
Manuel Botas <manuelbotas2001@gmail.com>
2023-10-27 00:54:16 -0300

    feat: sobreescritos los métodos de la interfaz DAO en UsuarioDAO

--- src/main/java/com/mycompany/sistema/autogestion/java/web/modelo/UsuarioDAO.java
+++ src/main/java/com/mycompany/sistema/autogestion/java/web/modelo/UsuarioDAO.java
@@ -4,10 +4,42 @@
  */
 package com.mycompany.sistema.autogestion.java.web.modelo;
 
+import java.util.List;
+
 /**
  *
  * @author Manuel Botas
  */
-public class UsuarioDAO {
+public class UsuarioDAO implements DAO<Usuario, Integer> {
+
+    @Override
+    public void insertar(Usuario entidad) throws Exception {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'insertar'");
+    }
+
+    @Override
+    public void modificar(Usuario entidad) throws Exception {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'modificar'");
+    }
+
+    @Override
+    public void eliminar(Usuario id) throws Exception {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'eliminar'");
+    }
+
+    @Override
+    public List<Usuario> listar() throws Exception {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'listar'");
+    }
+
+    @Override
+    public Usuario buscar(Integer id) throws Exception {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'buscar'");
+    }
     
 }
