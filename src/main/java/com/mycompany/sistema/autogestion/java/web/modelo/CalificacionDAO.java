6ae1fbdad589abb97e0bb8ae0ac55d12e1c136a5
Manuel Botas <manuelbotas2001@gmail.com>
2023-10-27 16:14:54 -0300

    feat: cambiado el método listar de CalificacionDAO para que use ConnectionPool

--- src/main/java/com/mycompany/sistema/autogestion/java/web/modelo/CalificacionDAO.java
+++ src/main/java/com/mycompany/sistema/autogestion/java/web/modelo/CalificacionDAO.java
@@ -4,7 +4,12 @@
  */
 package com.mycompany.sistema.autogestion.java.web.modelo;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList; 
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -29,8 +34,19 @@ public class CalificacionDAO implements	DAO<Calificacion, Integer> {
     }
 
     @Override
-    public List<Calificacion> listar() throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public List<Calificacion> listar() {
+        List<Calificacion> calificaciones = new LinkedList<>();
+        String query = "SELECT * FROM calificacion";
+        try(Connection con = ConnectionPool.getInstance().getConnection();
+            PreparedStatement ps = con.prepareStatement(query);
+            ResultSet rs = ps.executeQuery()) {
+            while(rs.next()) {
+                calificaciones.add(rsToCalificacion(rs));
+            }
+        } catch (SQLException ex) {
+            throw new RuntimeException(ex);
+        }
+        return calificaciones;
     }
 
     @Override
