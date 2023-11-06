1f9d34a94785a9c65175ffe06603723f608741cf
Manuel Botas <manuelbotas2001@gmail.com>
2023-11-06 00:19:52 -0300

    feat: agregado método doFilter a AuthFilter

--- src/main/java/com/mycompany/sistema/autogestion/java/web/filters/AuthFilter.java
+++ src/main/java/com/mycompany/sistema/autogestion/java/web/filters/AuthFilter.java
@@ -9,6 +9,10 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
 import java.io.IOException;
 
 /**
@@ -19,7 +23,16 @@ public class AuthFilter implements Filter{
 
     @Override
     public void doFilter(ServletRequest sr, ServletResponse sr1, FilterChain fc) throws IOException, ServletException {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        HttpServletRequest httpReq = (HttpServletRequest) sr;
+        HttpServletResponse httpResp = (HttpServletResponse) sr1;
+        HttpSession session = httpReq.getSession();
+        if(session != null && session.getAttribute("userLogueado") != null){
+            fc.doFilter(sr, sr1);
+        } else {
+            httpReq.setAttribute("hayError", true);
+            httpReq.setAttribute("mensajeError", "Debe iniciar sesión");
+            httpResp.sendRedirect(httpReq.getContextPath());
+        }
     }
     
 }
