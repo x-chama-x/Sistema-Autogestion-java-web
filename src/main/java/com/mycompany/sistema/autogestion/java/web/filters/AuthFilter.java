c5fd29535b3bfcebd5a2376bcbeac08466fcda0c
Manuel Botas <manuelbotas2001@gmail.com>
2023-11-05 22:18:00 -0300

    feat: agregado paquete filters; agregado filtro AuthFilter

---
+++ src/main/java/com/mycompany/sistema/autogestion/java/web/filters/AuthFilter.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.sistema.autogestion.java.web.filters;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import java.io.IOException;
+
+/**
+ *
+ * @author Manuel Botas
+ */
+public class AuthFilter implements Filter{
+
+    @Override
+    public void doFilter(ServletRequest sr, ServletResponse sr1, FilterChain fc) throws IOException, ServletException {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
