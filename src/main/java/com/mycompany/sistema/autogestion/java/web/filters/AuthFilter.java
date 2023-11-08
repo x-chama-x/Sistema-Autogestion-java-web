021ef8b426b7d56ed9aae66d52939a4728253f55
Manuel Botas <manuelbotas2001@gmail.com>
2023-11-08 19:05:22 -0300

    fix: arreglado método doFilter de AuthFilter; arreglado el mapeo de AuthFilter en web.xml

--- src/main/java/com/mycompany/sistema/autogestion/java/web/filters/AuthFilter.java
+++ src/main/java/com/mycompany/sistema/autogestion/java/web/filters/AuthFilter.java
@@ -26,11 +26,8 @@ public class AuthFilter implements Filter{
         HttpServletRequest httpReq = (HttpServletRequest) sr;
         HttpServletResponse httpResp = (HttpServletResponse) sr1;
         HttpSession session = httpReq.getSession();
-        String requestURI = httpReq.getRequestURI();
         String contextPath = httpReq.getContextPath();
-        if(!requestURI.equals(contextPath)) {
-            fc.doFilter(sr, sr1);
-        } else if(session != null && session.getAttribute("userLogueado") != null) {
+        if(session != null && session.getAttribute("userLogueado") != null) {
             fc.doFilter(sr, sr1);
         } else {
             httpReq.setAttribute("hayError", true);
