6ff84841265f0774f4b5f8233d14553664f99ed9
Manuel Botas <manuelbotas2001@gmail.com>
2023-11-16 21:04:51 -0300

    chore: eliminados comentarios innecesarios de LoginServlet

--- src/main/java/com/mycompany/sistema/autogestion/java/web/controlador/LoginServlet.java
+++ src/main/java/com/mycompany/sistema/autogestion/java/web/controlador/LoginServlet.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
- */
+
 package com.mycompany.sistema.autogestion.java.web.controlador;
 
 import jakarta.servlet.ServletException;
@@ -35,7 +32,6 @@ public class LoginServlet extends HttpServlet {
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
@@ -48,7 +44,6 @@ public class LoginServlet extends HttpServlet {
         }
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
      *
@@ -120,6 +115,6 @@ public class LoginServlet extends HttpServlet {
     @Override
     public String getServletInfo() {
         return "Short description";
-    }// </editor-fold>
+    }
 
 }
