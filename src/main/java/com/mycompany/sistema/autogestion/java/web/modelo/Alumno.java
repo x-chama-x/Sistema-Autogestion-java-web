725e46f4e47a6ea2164626b4b7f2127b4bd66c57
Manuel Botas <manuelbotas2001@gmail.com>
2023-11-16 22:18:33 -0300

    feat: agregado un nuevo constructor a la clase Alumno

--- src/main/java/com/mycompany/sistema/autogestion/java/web/modelo/Alumno.java
+++ src/main/java/com/mycompany/sistema/autogestion/java/web/modelo/Alumno.java
@@ -8,13 +8,16 @@ package com.mycompany.sistema.autogestion.java.web.modelo;
 public class Alumno extends Usuario {
     private int idAlumno;
     private int idCursada;
-
-    // TODO Agregar atributo idCursada al constructor
-    public Alumno(int idAlumno,/* int idCursada,*/ int idUsuario, String nombre, String apellido, String email, String contrasenia, Estado estado) {
+    
+    public Alumno(int idAlumno, int idUsuario, String nombre, String apellido, String email, String contrasenia, Estado estado) {
         super(idUsuario, nombre, apellido, email, contrasenia, estado);
         this.idAlumno = idAlumno;
-        //this.idCursada = idCursada;
+    }
 
+    public Alumno(int idAlumno, int idCursada, int idUsuario, String nombre, String apellido, String email, String contrasenia, Estado estado) {
+        super(idUsuario, nombre, apellido, email, contrasenia, estado);
+        this.idAlumno = idAlumno;
+        this.idCursada = idCursada;
     }
 
     public int getIdAlumno() {
